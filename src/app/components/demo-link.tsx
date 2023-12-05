37306850a515b3753e12ec474ed39ff8fdc6e690
Riku Putkinen <putkinenriku@gmail.com>
2023-12-05 22:19:30 +0200

    Fix CSS

--- src/app/components/demo-link.tsx
+++ src/app/components/demo-link.tsx
@@ -3,15 +3,15 @@ import Image, { StaticImageData } from "next/image";
 
 export default function DemoLink({ imageSrc, linkURL, linkText } : { imageSrc: StaticImageData, linkURL: string, linkText: string }) {
   return (
-    <div className="border border-neutral-700 w-fit">
-      <Link href={linkURL}>
+    <div className="border border-neutral-700 w-fit flex flex-col">
+      <Link href={linkURL} className="flex-grow flex justify-center items-center">
         <Image
           src={imageSrc}
           width={400}
           height={300}
           alt="" />
-        <p className="underline bg-neutral-900 p-2">{linkText}</p>
       </Link>
+        <Link href={linkURL} className="underline bg-neutral-900 p-2">{linkText}</Link>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import Link from 'next/link'
+"use client"
+
 import DemoLink from './components/demo-link'
 import pricingThumbnail from '../../public/pricing-img.png'
+import shopThumbnail from '../../public/shop-img.png'
 import { StaticImageData } from 'next/image'
 
 interface DemoDataObj {
@@ -14,6 +16,11 @@ const demoData: DemoDataObj[] = [
     imageSrc: pricingThumbnail,
     linkURL: "/pricing",
     linkText: "Pricing"
+  },
+  {
+    imageSrc: shopThumbnail,
+    linkURL: "/shop",
+    linkText: "Shop"
   }
 ]
 
@@ -23,7 +30,7 @@ export default function Home() {
       <h1 className='text-3xl text-center'>Small demo pages</h1>
       <p className='text-center'>One page projects grouped in this website.</p>
 
-      <div>
+      <div className='grid gap-4 mt-8 grid-cols-[repeat(auto-fill,_400px)]'>
         {demoData.map(demo => {
           return <DemoLink imageSrc={demo.imageSrc} linkURL={demo.linkURL} linkText={demo.linkText} key={demo.linkText} />
         })}
